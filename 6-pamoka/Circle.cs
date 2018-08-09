2e634dcb0255759ebce2e8f7febfd25bd95847e8
Rita <rita@trafi.com>
2018-08-09 16:36:30 +0300

    Add files via upload

---
+++ 6-pamoka/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SestaPamoka
+{
+    public class Circle
+    {
+        private double _radius = 1;
+        private string color = "red";
+
+        public Circle()
+        {
+        }
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double GetArea() 
+        {
+            return Math.PI * _radius * _radius;
+        }
+
+        public double GetLength()
+        {
+            return 2 * Math.PI * _radius;
+        }
+    }
+}
